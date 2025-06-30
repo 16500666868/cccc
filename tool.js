0b811fff4f29e34daaaa1f03b956e97d4806ba48
xiaohhh <68457850+16500666868@users.noreply.github.com>
2025-06-30 15:18:04 +0800

    Update tool.js

--- tool.js
+++ tool.js
@@ -23,39 +23,17 @@ window.thispagehasfinshed=function (){
 var nextpage=function (){
     document.querySelector(".rc-pagination-next").click();
 }
-var tooldiv = document.createElement("div");
-tooldiv.style.position = "fixed";
-tooldiv.style.top = "10px";
-tooldiv.style.right = "10px";
-tooldiv.style.width = "110px";
-tooldiv.style.height = "140px";
-tooldiv.style.backgroundColor = "#4fc4e5";
-tooldiv.style.zIndex = '99999';
-tooldiv.style.borderRadius = "8px"; // 圆角边框
-tooldiv.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)"; // 添加阴影
-tooldiv.style.padding = "10px"; // 内边距
-tooldiv.style.fontFamily = "Arial, sans-serif"; // 设置字体
-document.querySelector("body > div > main > div").append(tooldiv);
-tooldiv.innerHTML="<h1 style=\"width: 100%; text-align: center; font-size: larger; margin: 0;\">辅助</h1>\n" +
-    "<button id=\"thispageall\" class=\"tool-button\">一键此页+阻止弹窗</button><br>" +
-    "<button id=\"nextall\"  class=\"tool-button\"  >下一页</button>\n"+
-"\n<style>" +
-    "    .tool-button {\n" +
-    "        width: 100%;\n" +
-    "        margin: 5px 0;\n" +
-    "        padding: 10px;\n" +
-    "        background-color: #336600;\n" +
-    "        color: white;\n" +
-    "        border: none;\n" +
-    "        border-radius: 4px;\n" +
-    "        cursor: pointer;\n" +
-    "        font-size: 10px;\n" +
-    "        transition: background-color 0.3s ease;\n" +
-    "    }\n" +
-    "\n" +
-    "    .tool-button:hover {\n" +
-    "        background-color: #254d00;\n" +
-    "    }\n" +
-    "</style>";
+var button1 = document.createElement("button");
+var button2 = document.createElement("button");
+button1.id = "thispageall";
+button2.id = "nextall";
+button1.className = "w-full select-none rounded-lg leading-5 transition-all duration-300 font-normal px-4 py-[10px] text-[14px] sm:px-[24px] sm:py-[10px] border backdrop-blur-[9px] dark:bg-black/30 dark:shadow-button-inset border-purple-contrast/50 hover:bg-purple-dark/10 dark:border-purple-dark/15 dark:text-purple-light dark:shadow-button-purple/10 dark:hover:shadow-button-purple/20";
+button2.className = "w-full select-none rounded-lg leading-5 transition-all duration-300 font-normal px-4 py-[10px] text-[14px] sm:px-[24px] sm:py-[10px] border backdrop-blur-[9px] dark:bg-black/30 dark:shadow-button-inset border-purple-contrast/50 hover:bg-purple-dark/10 dark:border-purple-dark/15 dark:text-purple-light dark:shadow-button-purple/10 dark:hover:shadow-button-purple/20";
+button1.style.width = "225px";
+button2.style.width = "225px";
+button1.innerHTML = "本页全部+拦截弹窗";
+button2.innerHTML = "下一页";
+document.querySelector("body > div > main > div > div > div.py-2.opacity-80.lg\\:py-6").prepend(button1);
+document.querySelector("body > div > main > div > div > div.py-2.opacity-80.lg\\:py-6").prepend(button2);
 document.getElementById("thispageall").addEventListener("click", clickall);
 document.getElementById("nextall").addEventListener("click", nextpage);
